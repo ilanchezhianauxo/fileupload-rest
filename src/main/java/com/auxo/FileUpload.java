44486a10198a016334a188930d3deb4b91bd0043
ilanchezhian <ilanchezhianj.qa@auxolabs.in>
2017-12-07 11:06:44 +0530

    second push

--- src/main/java/com/auxo/FileUpload.java
+++ src/main/java/com/auxo/FileUpload.java
@@ -1,4 +1,10 @@
 package com.auxo;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class FileUpload {
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+    }
 }
